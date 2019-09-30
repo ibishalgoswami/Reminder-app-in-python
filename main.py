1e0e5ed5a6edae18f8f69ea3be9a703115a089ab
Bishal <ibishalgoswami@gmail.com>
2019-09-30 18:06:58 +0530

    Changes have been made

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from win10toast import ToastNotifier
 import re
 
 #myFile = 'C:/Users/Desktop/New folder/containerfile'
-myFile = 'path to your file'
+
+URL = 'https://www.way2sms.com/api/v1/sendCampaign'
+
 def checkTodaysBirthdays():
     while True:
         fileName = open(myFile, 'r')
@@ -13,13 +15,12 @@ def checkTodaysBirthdays():
             if today in line:
                 result = re.sub(r'[:]', r'', line)
                 result2 = ''.join(i for i in result if not i.isdigit())
-
                 flag = 1
-                if flag == 1:
-                    notifier = ToastNotifier()
-                    notifier.show_toast("To Do", result2, duration=16)
+        if flag == 1:
+            notifier = ToastNotifier()
+            notifier.show_toast("To Do", result2, duration=16)
+        else:
+            print("Nothing to show")
+            break
 
 checkTodaysBirthdays()
-
-
-
